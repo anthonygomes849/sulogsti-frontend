90668b0600fa4afa639e8870f16494eab802a747
anthonygomes849 <anthonygomes894@gmail.com>
2024-10-17 20:32:17 -0300

    Update

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const app = express();
+
+// Para trabalhar com __dirname em ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Serve os arquivos estáticos do build
 app.use(express.static(path.join(__dirname, 'dist')));
 
+// Redireciona todas as outras rotas para o index.html
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
